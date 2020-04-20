824d2f23f09d164a543825b75164f31af04c2f55
JoBite1235 <63899068+JoBite1235@users.noreply.github.com>
2020-04-20 10:28:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Store form data in .txt file</title>
-</head>
-<body>
-  <form method="post">
-    Enter Your Text Here:<br>
-    <input type="text" name="textdata"><br>
-    <input type="submit" name="submit">
-    
-  </form>
-</body>
-</html>
+
 <?php
               
 if(isset($_POST['textdata']))
