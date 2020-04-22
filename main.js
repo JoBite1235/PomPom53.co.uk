b5579708b0bdd30b115e911b96f852b3857ade51
JoBite1235 <63899068+JoBite1235@users.noreply.github.com>
2020-04-22 07:52:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-const canvas = document.getElementById("screen");
-const ctx = canvas.getContext("2d");
+var canvas = document.getElementById("screen");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-let x = 0;
-let y = 0;
-
-function drawBall(){
-  ctx.fillStyle = "#0000FF";
-  ctx.fillRect(x, y, 100, 100);
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
 
-function draw(){
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-  drawBall()
-  if (y++ > canvas.height){
-    y--;
+  drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
   }
-  else{
-  y++;
-  x++;
-    
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
   }
-    
-  
-  
+
+  x += dx;
+  y += dy;
 }
 
 setInterval(draw, 10);
