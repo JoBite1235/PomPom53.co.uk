ef70c84cd5ba99460189a27f1e425fb2a6cf10e9
JoBite1235 <63899068+JoBite1235@users.noreply.github.com>
2020-05-06 17:41:21 +0100

    Add files via upload

--- logincheck.js
+++ logincheck.js
@@ -1,3 +1,4 @@
-if(!(sessionStorage.getItem(login) == 1)){
+if(!(localStorage.getItem(login) == 1)){
     window.location.assign("https://pompom53.co.uk/index.html")
+    localStorage.setItem(login, 0)
 }
