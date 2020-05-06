162cb4bd65eba89502da75d3515b28687187f8b5
JoBite1235 <63899068+JoBite1235@users.noreply.github.com>
2020-05-06 17:31:02 +0100

    Add files via upload

--- login.js
+++ login.js
@@ -5,6 +5,7 @@ input = parseInt(document.querySelector("#pass").value)
 
 if (input == 2904){
     window.location.assign('https://pompom53.co.uk/logedin.html');
+    sessionStorage.setItem(login, 1);
 
 }
 
---
+++ logincheck.js
@@ -0,0 +1,3 @@
+if(!(sessionStorage.getItem(login) == 1)){
+    window.location.assign("https://pompom53.co.uk/index.html")
+}
